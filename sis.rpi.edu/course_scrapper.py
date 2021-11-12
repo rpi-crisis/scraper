41dcf7c4434e7fbb149a41eb18d404613cae4dea
maryellenmarino <74571866+maryellenmarino@users.noreply.github.com>
2021-11-12 16:37:03 -0500

    fixed searches for SIS scraper

--- sis.rpi.edu/course_scrapper.py
+++ sis.rpi.edu/course_scrapper.py
@@ -40,7 +40,7 @@ search_data = {
 # accesses each search correctly
 # still needs to obtain data from each search
 with rq.Session() as s:
-        url = 'https://sis.rpi.edu/rss/bwckctlg.p_display_courses?term_in=202109&sel_crse_strt=0&sel_crse_end=9999&sel_subj=&sel_levl=&sel_schd=&sel_coll=&sel_divs=&sel_dept=&sel_attr='
+        url = 'https://sis.rpi.edu/rss/bwckctlg.p_display_courses?term_in=202101&sel_crse_strt=0&sel_crse_end=9999&sel_subj=&sel_levl=&sel_schd=&sel_coll=&sel_divs=&sel_dept=&sel_attr='
         r = rq.get(url, headers=headerss)
         soup = bs(r.text, 'html.parser')
         value = soup.find('select', {"name": "sel_subj"})
@@ -54,13 +54,16 @@ with rq.Session() as s:
                 search_data['sel_subj'] = search
                 r = s.post(url, data=search_data, headers=headerss)
                 innerSoup = bs(r.text,'html.parser')
-                value = soup.find('tbody')
-                class_title = soup.find_all('td', attrs={'class':'nttitle'})
-                class_info = soup.find_all('td', attrs={'class':'ntdefault'})
-                # fix that ^^^^
+                value = innerSoup.find('tbody')
+                class_title = innerSoup.find_all('td', attrs={'class':'nttitle'}, recursive = True)
+                class_info = innerSoup.find_all('td', attrs={'class':'ntdefault'}, recursive = True)
+                if search_data['sel_subj'] == "ADMN":
+                        print(class_title)
                 '''
                 As of right now, this will get relevant information in each of 
                 the searches possible. The next step is to only collect data that
                 has a link under the type of class, then gather data from that 
                 particular link.
                 '''
+
+
