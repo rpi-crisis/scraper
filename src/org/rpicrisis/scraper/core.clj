9523e9583f25b2ec1ebee8b35b22eb416dc0aa23
Lily Ritter <lily@lilychan.dev>
2022-02-15 16:40:39 -0500

    Clean up Clojure, it was a fun experiment while it lasted

--- src/org/rpicrisis/scraper/core.clj
+++
@@ -1,9 +0,0 @@
-(ns org.rpicrisis.scraper.core)
-
-;; (defn -main []
-;;   ;; (doseq [school schools]
-;;   ;;   (prn school))
-;;   (doseq [school subjects]
-;;     (println (schools (key school)))
-;;     (doseq [subject (val school)]
-;;       (println (str "  " (-> subject key name upper-case) " - " (val subject))))))
