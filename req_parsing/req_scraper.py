406b326c4314d8c3556a24dd39fa6261ef396a51
droolz <maximhutz@gmail.com>
2022-02-18 17:14:41 -0500

    Updated req. scraper. Ran output into .

--- req_parsing/req_scraper.py
+++ req_parsing/req_scraper.py
@@ -5,6 +5,8 @@ import re, json, sys, os, json
 W_CATALOG = "http://catalog.rpi.edu/content.php?"
 W_COURSE = "http://catalog.rpi.edu/preview_course.php?"
 
+file = open("reqs.txt", "w", buffering=1)
+
 
 def get_page(page):
 	return W_CATALOG + f"catoid=22&navoid=544&filter[cpage]={page}"
@@ -15,20 +17,30 @@ def get_course(course_id):
 
 
 def parse_course_reqs(coid):
+	global file
+
 	page = get_course(coid)
 
 	html = rq.get(page).content.decode('utf-8')
 	soup = bs(html, 'html.parser')
 
 	content = soup.find("td", { "class": "block_content_popup" }, recursive=True)
-	desc_parts = content.find_all()
 
-	for part in desc_parts:
-		if part.name == "strong":
-			if "Prerequisites/Corequisites:" in part.text:
-				return part.next_sibling
+	parts = [part for part in content.contents]
 
-	return None
+	reqs = None
+
+
+	for part in parts:
+		if reqs == None:
+			if part.name == "strong" and "Prerequisites/Corequisites:" in part.get_text():
+				reqs = ""
+		elif part.name == "strong":
+			return file.write(reqs.strip().replace(u'\xa0', ' ') + '\n')
+		elif part.name == None:
+		  reqs += part
+		else:
+			reqs += part.get_text()
 
 
 def parse_page(n):
@@ -45,22 +57,26 @@ def parse_page(n):
 	course_list = course_table.find_all("tr")[1:-2]
 
 	for course in course_list:
-		link = course.find("td", { "class": "width" }).a['href']
+		link = course.find("td", { "class": "width" }).a
 
-		coid = link.split("coid=")[1]
-		print(coid)
+		print(link['title']);
 
-		req = parse_course_reqs(coid)
-		if req is not None: reqs.append(req)
+		coid = link['href'].split("coid=")[1]
+
+		parse_course_reqs(coid)
 
 	return reqs
 
 def main():
 	reqs = []
 
-	reqs.extend(parse_page(1))
 
-	for req in reqs:
-		print(req)
+	for i in range(20):
+		print(f"========== Page {i + 1} / 20 ==========");
+
+		parse_page(i + 1)
+
 
 if __name__ == '__main__': main()
+
+file.close()
