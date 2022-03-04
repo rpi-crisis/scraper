c55d2a9c666227f0ee3030d72e89e708fa622f10
akeylg <graya4@rpi.edu>
2022-03-04 17:02:24 -0500

    started to implement further scraper

--- src/baccalaureate_scraper.py
+++ src/baccalaureate_scraper.py
@@ -10,7 +10,10 @@ ba_elements = major_elements[0].find_all("a")
 for z in ba_elements:
     major_title = z.text.strip()
     major_link = z.get('href')
-    baccalaureate[major_title] = {"description": " ", "requirements": " "}
+    baccalaureate[major_title] = {"description": " ", "requirements": " ", 
+    "years": [[], [], [], []], 
+    "other-content": {}}
+    baccalaureate[major_title]["other-content"] = {"options": " ", "capstone": " ", "transfer_policy": " ", "footnotes": " "}
     major_webpage = requests.get("http://catalog.rpi.edu/" + major_link)
     major_soup = BeautifulSoup(major_webpage.content, 'html5lib')
     major_table = major_soup.find("td", class_="block_content")
@@ -19,4 +22,13 @@ for z in ba_elements:
     description_tr = trs[0]
     requirement_tr = trs[3]
     baccalaureate[major_title]["description"] = description_tr
-    baccalaureate[major_title]["requirements"] = requirement_tr    
+    baccalaureate[major_title]["requirements"] = requirement_tr   
+    yearsHTML = requirement_tr.find("div", class_="custom_leftpad_20")
+    yeartitlelist = yearsHTML.find_all("div", class_ = "acalog-core")
+    yeardescriptionlist = yearsHTML.find_all("div", class_="custom_leftpad_20")
+    for x in range(0, len(yeartitlelist)):
+        baccalaureate[major_title]["years"][x].add(yeartitlelist[x])
+        baccalaureate[major_title]["years"][x].add(yeardescriptionlist[x])
+    
+    print("----------------------")
+    break
