3eef023c0aa4f3dbb3f66737a9922e9b670dc714
akeylg <graya4@rpi.edu>
2022-03-22 16:57:55 -0400

    fixed some stuff and implemented footnotes

--- src/baccalaureate_scraper.py
+++ src/baccalaureate_scraper.py
@@ -1,5 +1,6 @@
-from tracemalloc import start
+from cgi import test
 from bs4 import BeautifulSoup
+from numpy import equal
 import requests
 r = requests.get("http://catalog.rpi.edu/content.php?catoid=22&navoid=542")
 baccalaureate = {}
@@ -7,13 +8,16 @@ soup = BeautifulSoup(r.content, 'html5lib')
 table = soup.find("td", class_="block_content")
 major_elements = table.find_all("ul", class_="program-list")
 ba_elements = major_elements[0].find_all("a")
+
 for z in ba_elements:
     major_title = z.text.strip()
     major_link = z.get('href')
+    
     baccalaureate[major_title] = {"description": " ", "requirements": " ", 
     "years": [[], [], [], [], []], 
     "other-content": {}}
     baccalaureate[major_title]["other-content"] = {"options": " ", "capstone": " ", "transfer_policy": " ", "footnotes": " "}
+    
     major_webpage = requests.get("http://catalog.rpi.edu/" + major_link)
     major_soup = BeautifulSoup(major_webpage.content, 'html5lib')
     major_table = major_soup.find("td", class_="block_content")
@@ -24,20 +28,30 @@ for z in ba_elements:
     baccalaureate[major_title]["description"] = description_tr
     baccalaureate[major_title]["requirements"] = requirement_tr   
     yearsHTML = requirement_tr.find("div", class_="custom_leftpad_20")
+    
     if yearsHTML == None:
         continue 
     yearlist = yearsHTML.find("div", class_ = "acalog-core")
     count = 0
+    yearcount = 0
+    print(yearsHTML)
+    print("---------------------------")
     while(yearlist != None):
-        if count > 3:
-            break
+        print(yearlist)
+        try:
+            testString = yearlist.h2.a
+            if "Footnotes" in testString:
+                baccalaureate[major_title]["other_content"]["footnotes"].append(yearlist)
+        except:
+            print("no h2")
         baccalaureate[major_title]["years"][count].append(yearlist)
-        baccalaureate[major_title]["years"][count].append(yearlist.next_sibling)
-        if yearlist.next_sibling == None or yearlist.next_sibling.next_sibing == None:
-            break
-        yearlist = yearlist.next_sibling.next_sibling
-        count += 1
+        yearlist = yearlist.next_sibling
+        yearcount += 1
+        if yearcount > 1:
+            yearcount = 0
+            count += 1
+        print("-------")
     
-    
+    #print(baccalaureate[major_title]["years"])
     print("----------------------")
     break
