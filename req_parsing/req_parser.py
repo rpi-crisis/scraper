ee3a901d5e75873bcd4416ba404bfe0e537a7452
droolz <maximhutz@gmail.com>
2022-03-22 17:07:02 -0400

    Started working with lark to generate parser for created grammars.

--- req_parsing/req_parser.py
+++ req_parsing/req_parser.py
@@ -1,148 +1,28 @@
-import re, json, sys, os, json
-from fuzzywuzzy import fuzz
+from lark import Lark
 
-reqs_file = open("reqs.txt", "r")
-coid_file = open("coid.json", "r")
+grammar_file = open("req_grammar.lark", "r")
+grammar = grammar_file.read()
 
-out_file = open("parsing2.txt", "w")
+req_file = open("parsing2.txt", "r")
+lines = req_file.readlines()
 
-coids = json.load(coid_file)
+out_file = open("parsing3.txt", "w")
 
-crse_regex = re.compile(r"[A-Z]{4}[\- ]?\d{4}")
+parser = Lark(grammar)
 
-parsing_spec = [
-	('AND', r"\band\b"),
-	('NOT', r"\bnot\b"),
-	('OR', r"\bor\b"),
-	('INS', r"instructor"),
-	('GRD', r"graduate"),
-	('SEN', r"senior"),
-	('JUN', r"junior"),
-	('SOF', r"sophomore"),
-	('FRS', r"freshman"),
-	('REC', r"recommend"),
-	('REQ', r"require"),
-	('RST', r"restrict"),
-	('EXC', r"except"),
-	('ARC', r"m\.arch\."),
-	('CID', r"\d{4}"),
-	('DPT', r"\b[A-Z]{4}\b"),
-	('PRE', r"prerequisite"),
-	('COR', r"corequisite"),
-	('CRS', r"course"),
-]
+total = 0
+good = 0
 
-parse_regex = re.compile('|'.join(f"(?P<{pair[0]}>{pair[1]})" for pair in parsing_spec), re.I)
+for line in lines:
+	try:
+		total += 1
+		text = parser.parse(line).pretty() + "\n\n\n\n\n"
+		good += 1
+		out_file.write(text)
+	except:
+		pass
 
-# Determines if a string is close enough to be considered a match.
-# NOTE: 80% or greater is considered a match.
-def cmatch(str1, str2):
-	return fuzz.partial_ratio(str1, str2) > 80
-
-
-def cut_class_names(line):
-	global coids
-
-	offset = 0
-	result = ""
-
-	matches = crse_regex.finditer(line)
-	
-	for match in matches:
-		crse = match[0]
-
-		if crse[4] == '-':
-			crse = crse[:4] + ' ' + crse[5:]
-			line = line[:match.start() + 4] + ' ' + line[match.start() + 5:]
-		elif len(crse) == 8:
-			crse = crse[:4] + ' ' + crse[4:]
-			line = line[:match.start() + 4] + ' ' + line[match.start() + 4:]
-
-		name = ""
-
-		try: name = coids[crse]
-		except KeyError: continue
-
-		next_start = match.end() + 1
-		next_end = next_start + len(name)
-
-		if len(line) <= next_end:
-			next_end = len(line) - 1
-
-		next_txt = line[next_start : next_end]
-
-		if cmatch(next_txt, name):
-			result += " " + line[offset : next_start - 1]
-			offset = next_end
-
-	return (result + " " + line[offset:]).strip()
-
-
-def tokenize(line):
-	global parse_regex
-
-	tokens = []
-
-	matches = parse_regex.finditer(line)
-
-	for match in matches:
-		gdict = match.groupdict()
-
-		for group in gdict:
-			txt = gdict[group]
-
-			if txt is not None:
-				if group == 'DPT' and not txt.isupper(): continue
-				tokens.append((group, txt))
-
-	return tokens
-
-def txt_token(token):
-	tok_type, tok_txt = token
-
-	if 	 tok_type == "AND": return  "{AND}"
-	elif tok_type == "OR":  return  "{OR}"
-	elif tok_type == "NOT": return  "{NOT}"
-	elif tok_type == "INS": return  "<INSTRUCTOR>"
-	elif tok_type == "SEN": return  "<SENIOR>"
-	elif tok_type == "JUN": return  "<JUNIOR>"
-	elif tok_type == "SOF": return  "<SOPHOMORE>"
-	elif tok_type == "FRS": return  "<FRESHMAN>"
-	elif tok_type == "REC": return  "<RECOMMEND>"
-	elif tok_type == "REQ": return  "<REQUIRE>"
-	elif tok_type == "RST": return  "<RESTRICT>"
-	elif tok_type == "EXC": return  "<EXCEPT>"
-	elif tok_type == "ARC": return  "<M.ARCH.>"
-	elif tok_type == "DPT": return f"({tok_txt})"
-	elif tok_type == "CID": return f"({tok_txt})"
-	elif tok_type == "PRE": return  "[PREREQ]"
-	elif tok_type == "COR": return  "[COREQ]"
-	elif tok_type == "GRD": return  "<GRADUATE>"
-	elif tok_type == "CRS": return  "<COURSE>"
-
-	return None
-
-def parse(line):
-	global out_file
-
-	line2 = cut_class_names(line)
-
-	tokens = tokenize(line2)
-
-	txt = " ".join(txt_token(token) for token in tokens)
-
-	out_file.write(txt + "\n")
-
-
-def main():
-	lines = reqs_file.readlines()
-
-	for line in lines:
-		parse(line)
-
-if __name__ == '__main__': main()
-
-reqs_file.close()
-coid_file.close()
-
+print(f"Grammar completed: {good} / {total}, {good / total * 100}%")
 out_file.close()
+req_file.close()
+grammar_file.close()
